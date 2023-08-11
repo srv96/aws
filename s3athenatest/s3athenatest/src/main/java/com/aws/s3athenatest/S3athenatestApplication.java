000d44a35095a85945c79047e2c378064eb1fe83
Sourav kumar Dash <33924763+srv96@users.noreply.github.com>
2023-08-11 15:58:58 +0530

    Update S3athenatestApplication.java

--- s3athenatest/s3athenatest/src/main/java/com/aws/s3athenatest/S3athenatestApplication.java
+++ s3athenatest/s3athenatest/src/main/java/com/aws/s3athenatest/S3athenatestApplication.java
@@ -1,46 +1,36 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-#!/bin/bash
+public class CsvExporter {
+    public static void exportToCsv(List<List<String>> data, String filePath) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (List<String> row : data) {
+                StringBuilder csvRow = new StringBuilder();
+                for (String cell : row) {
+                    csvRow.append(cell).append(",");
+                }
+                csvRow.deleteCharAt(csvRow.length() - 1); // Remove trailing comma
+                csvRow.append("\n");
+                writer.write(csvRow.toString());
+            }
+        }
+    }
 
-# Check if the user provided the input path
-if [ $# -eq 0 ]; then
-  echo "Please provide a Hadoop file path."
-  exit 1
-fi
+    public static void main(String[] args) {
+        List<List<String>> data = new ArrayList<>();
+        data.add(List.of("John", "Doe", "30"));
+        data.add(List.of("Jane", "Smith", "25"));
+        data.add(List.of("Alice", "Johnson", "28"));
 
-input_path=$1
-result_file="result.txt"
-declare -A visited_dirs
+        String filePath = "output.csv";
 
-# Function to process each directory and write results to result.txt
-function process_directory() {
-  local directory_path=$1
-
-  # Check if the directory has been visited before
-  if [[ ${visited_dirs["$directory_path"]} ]]; then
-    return
-  fi
-
-  # Mark the directory as visited
-  visited_dirs["$directory_path"]=1
-
-  # Get the number of files and total size of the directory
-  local file_count=$(hdfs dfs -ls "$directory_path" | grep '^-' | wc -l)
-  local dir_size=$(hdfs dfs -du -s -h "$directory_path" | awk '{print $1}')
-
-  # Write results to result.txt
-  echo "Directory: $directory_path, Number of Files: $file_count, Size: $dir_size" >> "$result_file"
-
-  # Recursively process subdirectories
-  local subdirs=$(hdfs dfs -ls "$directory_path" | grep '^d' | awk '{print $NF}')
-  for subdir in $subdirs; do
-    process_directory "$subdir"
-  done
+        try {
+            exportToCsv(data, filePath);
+            System.out.println("Data exported to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-# Clear the result file before starting
-> "$result_file"
-
-# Call the function with the input path
-process_directory "$input_path"
-
-echo "Results have been stored in $result_file"
