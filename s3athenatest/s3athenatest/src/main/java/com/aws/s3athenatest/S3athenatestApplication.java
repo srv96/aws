98a690a4113991181b7d49a985563b27ab7a8e7c
Sourav kumar Dash <33924763+srv96@users.noreply.github.com>
2023-08-11 18:45:17 +0530

    Update S3athenatestApplication.java

--- s3athenatest/s3athenatest/src/main/java/com/aws/s3athenatest/S3athenatestApplication.java
+++ s3athenatest/s3athenatest/src/main/java/com/aws/s3athenatest/S3athenatestApplication.java
@@ -1,51 +1,29 @@
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
 
-public class FileContentFilter {
-    public static List<File> filterFilesWithPattern(String directoryPath) {
-        List<File> filteredFiles = new ArrayList<>();
-        
-        File directory = new File(directoryPath);
-        if (!directory.isDirectory()) {
-            throw new IllegalArgumentException("Input path must be a directory.");
-        }
-        
-        File[] files = directory.listFiles();
-        if (files != null) {
-            Pattern pattern = Pattern.compile("part-\\d{5}-[A-Za-z0-9]{7}");
-            
-            for (File file : files) {
-                if (file.isFile()) {
-                    try {
-                        String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
-                        Matcher matcher = pattern.matcher(content);
-                        
-                        if (matcher.find()) {
-                            filteredFiles.add(file);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        
-        return filteredFiles;
+public class StringListManipulator {
+
+    public static void removeStrings(List<String> stringsToRemove, List<String> targetList) {
+        targetList.removeAll(stringsToRemove);
     }
-    
+
     public static void main(String[] args) {
-        String directoryPath = "path/to/your/directory";
-        List<File> filteredFiles = filterFilesWithPattern(directoryPath);
-        
-        for (File file : filteredFiles) {
-            System.out.println("Filtered File: " + file.getAbsolutePath());
-        }
+        List<String> list1 = new ArrayList<>();
+        list1.add("apple");
+        list1.add("banana");
+        list1.add("cherry");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("banana");
+        list2.add("grape");
+        list2.add("cherry");
+
+        System.out.println("List 1 before removal: " + list1);
+        System.out.println("List 2 before removal: " + list2);
+
+        removeStrings(list1, list2);
+
+        System.out.println("List 1 after removal: " + list1);
+        System.out.println("List 2 after removal: " + list2);
     }
 }
